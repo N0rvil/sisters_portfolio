2e7a0979afe383cb7ec038525622df2d084c21e2
Filip Duda <dudafilip80@seznam.cz>
2020-10-09 22:31:23 +0200

    pushing no responsive page with all components and with lot of images in albums

---
+++ album-1_script.js
@@ -0,0 +1,14 @@
+const albumBox = document.getElementById("album__box");
+let photoNum = 1;
+
+for (i = 0; i < 37; i++) { 
+    let a = document.createElement("a");
+    let img = document.createElement("img");
+    img.src = "img/first_album/" + photoNum + ".jpg";
+    img.alt = "photo"; 
+    a.href = "img/first_album/" + photoNum + ".jpg";
+    img.classList.add("album__photo");
+    albumBox.appendChild(a); 
+    a.appendChild(img);
+    photoNum++;
+  }
---
+++ album-3_script.js
@@ -0,0 +1,14 @@
+const albumBox = document.getElementById("album__box");
+let photoNum = 1;
+
+for (i = 0; i < 18; i++) { 
+    let a = document.createElement("a");
+    let img = document.createElement("img");
+    img.src = "img/third_album/" + photoNum + ".jpg"; 
+    img.alt = "photo";
+    a.href = "img/third_album/" + photoNum + ".jpg";
+    img.classList.add("album__photo");
+    albumBox.appendChild(a); 
+    a.appendChild(img);
+    photoNum++;
+  }
