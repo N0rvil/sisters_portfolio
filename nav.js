751926a4db80126143fd26b48ce875117f9c799d
Filip Duda <dudafilip80@seznam.cz>
2020-10-14 20:25:25 +0200

    pushing the responsive navbar with full functional hambureger

--- nav.js
+++ nav.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("hamburger");
 const list = document.getElementById("nav-list");
 const nav = document.getElementById("nav");
+const value = 900;
 let isNavActive = false;
     btn.onclick = () => {
         isNavActive = !isNavActive
@@ -12,6 +13,18 @@ let isNavActive = false;
         nav.style.height = "5rem";
     }
 }
+
+const resizing = () => { 
+    if (window.innerWidth > value) {
+        list.style.display = "grid";
+        nav.style.height = "5rem";
+    } 
+    
+}
+
+window.addEventListener("resize", resizing);
+
+
 
 
 
